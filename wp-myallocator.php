01432259d41cba8c6f5bc8603f9a828cf397f966
Kokonauta <lluesma@gmail.com>
2019-04-15 17:49:31 +0200

    Update Plugin Info

--- wp-myallocator.php
+++ wp-myallocator.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Plugin Name:     WP MyAllocator
- * Plugin URI:      PLUGIN SITE HERE
+ * Plugin URI:      https://github.com/kokonauta/WP-MyAllocator/
  * Description:     Integrate MyAllocator in WP
  * Author:          Jorge Lluesma Gil
- * Author URI:      www.jorgelluesma.com
+ * Author URI:      https://www.tutorialwp.net
  * Text Domain:     wp-myallocator
  * Domain Path:     /languages
  * Version:         0.1.0
