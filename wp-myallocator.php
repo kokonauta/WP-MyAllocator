49b2ecf754651e2d2beadfc57540343af59bdce8
Kokonauta <lluesma@gmail.com>
2019-04-15 17:35:52 +0200

    First commit

---
+++ wp-myallocator.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     WP MyAllocator
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Integrate MyAllocator in WP
+ * Author:          Jorge Lluesma Gil
+ * Author URI:      www.jorgelluesma.com
+ * Text Domain:     wp-myallocator
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Myallocator
+ */
+
+// Your code starts here.
